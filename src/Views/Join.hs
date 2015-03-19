6b9d3c1f95de2fccd643dfe487525bb2d028485b
Jonas Juselius <jonas.juselius@uit.no>
2015-03-19 09:03:20 +0100

    Error checking in join.

--- src/Views/Join.hs
+++ src/Views/Join.hs
@@ -6,14 +6,17 @@ module Views.Join (
     ) where
 
 import Prelude hiding (div, head, id)
+import Data.Monoid (mempty)
 import Text.Blaze (customAttribute)
 import Text.Blaze.Html
 import Text.Blaze.Html5 (
-    a, div, h1, h2, h3, nav, li, link, p, ul, (!), title, form, input, button)
+    a, div, h1, h2, h3, nav, li, link, p, ul, (!), title, form, input,
+    button, table, tr, td)
 import Text.Blaze.Html5.Attributes (
     class_, href, id, media, name, rel, src, type_, placeholder, action, method)
 import Views.Bootstrap
 import Web.Scotty
+import Network.HTTP.Types.Status
 import Data.Aeson (object, (.=))
 import qualified Data.Text as T
 
@@ -23,30 +26,54 @@ joinView = blaze $ bootstrap headerBasic $ do
     div ! class_ "container" $
         div ! class_ "starter-template" $ do
             h1 "tromsø haskell users group"
-            p "please join tromsø haskell users group!"
-            div ! class_ "container" $
-                div ! class_ "row" $ do
-                    h2 "thug"
-                    p "not really."
-                    form ! action "join" ! method "POST" $ do
-                        div ! class_ "form-group" $
-                            input   ! type_ "text"
-                                    ! placeholder "Email"
-                                    ! name "email"
-                                    ! class_ "form-control"
-                        div ! class_ "form-group" $
-                            input   ! type_ "password"
-                                    ! placeholder "Password"
-                                    ! name "passwd"
-                                    ! class_ "form-control"
-                        button  ! type_ "submit"
-                                ! class_ "btn btn-success" $ "Sign in"
+            p . toHtml $ unwords [
+                  "please join tromsø haskell users group!"
+                , "functional programmers can join too!"
+                ]
+            joinForm
     pageFooter
+
+joinForm :: Html
+joinForm = form ! action "join" ! method "POST" $
+    table ! class_ "table table-hover" $ do
+        tr $ do
+            td "full name"
+            td $ div ! class_ "form-group" $
+                input ! type_ "text"
+                      ! placeholder "Name"
+                      ! name "fullname"
+                      ! class_ "form-control"
+        tr $ do
+            td "email"
+            td $ div ! class_ "form-group" $
+                input ! type_ "text"
+                      ! placeholder "Email"
+                      ! name "email"
+                      ! class_ "form-control"
+        tr $ do
+            td "password"
+            td $ table ! class_ "table-condensed" $ do
+                tr . td $ div ! class_ "form-group" $
+                    input ! type_ "password"
+                          ! placeholder "Password"
+                          ! name "passwd1"
+                          ! class_ "form-control"
+                tr . td $ div ! class_ "form-group" $
+                        input ! type_ "password"
+                              ! placeholder "Verify Password"
+                              ! name "passwd2"
+                              ! class_ "form-control"
+        tr $ do
+            td mempty
+            td $ button ! type_ "submit"
+                   ! class_ "btn btn-success" $ "Register"
 
 joinHandler :: ActionM()
 joinHandler = do
-    emailAddress <- param "email" :: ActionM T.Text
-    passwd <- param "passwd" :: ActionM T.Text
+    fullname <- param "fullname" :: ActionM T.Text
+    email    <- param "email"    :: ActionM T.Text
+    passwd1  <- param "passwd1"  :: ActionM T.Text
+    passwd2  <- param "passwd2"  :: ActionM T.Text
     -- registered <- liftIO (registerInterest emailAddress)
     -- case registered of
     --     Just errorMessage -> do
@@ -54,14 +81,22 @@ joinHandler = do
     --         status internalServerError500
     --     Nothing -> do
     -- json $ object [ "ok" .= ("ok" :: String) ]
-    blaze $ bootstrap headerBasic $ do
-        navbar
-        div ! class_ "container" $
-            div ! class_ "starter-template" $ do
-                p $ toHtml emailAddress
-                p $ toHtml passwd
+    if passwd1 /= passwd2
+        then do
+            page $ p "Passwords don't match!"
+            status badRequest400
+        else page $ do
+            p $ toHtml fullname
+            p $ toHtml email
+            p $ toHtml . take (T.length passwd1) $ cycle ("*" :: String)
+        where
+            page x =
+                blaze $ bootstrap headerBasic $ do
+                    navbar
+                    div ! class_ "container" $
+                        div ! class_ "starter-template" $ x
 
 headerBasic :: Html
 headerBasic = do
     title "join thug"
-    link ! href "/custom/stater-template.css" ! rel "stylesheet"
+    link ! href "/custom/starter-template.css" ! rel "stylesheet"
